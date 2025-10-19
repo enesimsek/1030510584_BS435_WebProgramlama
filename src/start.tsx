4b12d80f03ff0c822c0befecaefc6323bd2106ec
enesimsek <simsekenes0580@gmail.com>
2025-10-19 23:07:29 +0300

    19.10.2025 Başla Sayfası Oluşturuldu

---
+++ src/start.tsx
@@ -0,0 +1,19 @@
+export const Start = () => {
+
+
+    return (
+        <div>
+            <button>
+                BAŞLA
+            </button>
+            <br />
+            <button>
+                AYARLAR
+            </button>
+            <br />
+            <button>
+                EMEĞİ GEÇENLER
+            </button>
+        </div>
+    )
+}
